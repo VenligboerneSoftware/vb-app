6bfa4e04ddd774360e41c622670f52f75583823f
benhannel <ben@bensprojects.com>
2017-08-19 22:23:13 +0200

    Broke apart PostList into two components to minimize rerendering

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,77 +1,46 @@
-import {
-	Dimensions,
-	FlatList,
-	ScrollView,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View
-} from 'react-native';
+import { View } from 'react-native';
 import Modal from 'react-native-modal';
 import React from 'react';
-import firebase from 'firebase';
 
-import { formatDate, getNextDate } from '../utils/dates';
-import { translate } from '../utils/internationalization';
-import EventIcon from './EventIcon.js';
 import PostOrCenterModal from './PostOrCenterModal';
-import SharedStyles from '../styles/SharedStyles';
 import ViewApplications from './ViewApplications';
+import BarePostList from './BarePostList';
 
-export default class PostList extends React.Component {
-	constructor(props) {
-		super(props);
+export default class PostList extends React.PureComponent {
+	constructor() {
+		super();
 		this.state = {
 			isPostModalVisible: false,
-			selectedPost: null
+			isApplicationsModalVisible: false
 		};
 	}
 
 	_showModal = item =>
-		this.setState({ selectedPost: item, isPostModalVisible: true });
+		this.setState({
+			selectedPost: item,
+			isPostModalVisible: true
+		});
 
-	_hideModal = () => this.setState({ isPostModalVisible: false });
+	_hideModal = () =>
+		this.setState({
+			isPostModalVisible: false
+		});
 
-	// Rather inaccurate, but very fast, geodistance function
-	getDistance = (a, b) => {
-		const deltaLatitude = a.latitude - b.latitude;
-		const deltaLongitude =
-			(a.longitude - b.longitude) * Math.cos(a.latitude / 180.0 * Math.PI);
-		const totalDegrees = Math.sqrt(
-			deltaLatitude * deltaLatitude + deltaLongitude * deltaLongitude
-		);
-		// 111319 is the width of one degree latitude in meters
-		return totalDegrees * 111319;
-	};
+	_showApplications = post =>
+		this.setState({
+			isApplicationsModalVisible: true,
+			selectedPost: post
+		});
 
-	// Sort the posts by increasing distance from the mapRegion center, if specified.
-	// Use the users current location to label distance, but fall back on the
-	// map region center.
-	_sort = posts => {
-		// copy to avoid immutablility issues
-		console.log('Starting copy', Date.now());
-		posts = JSON.parse(JSON.stringify(posts));
-		console.log('Copy complete', Date.now());
-		if (this.props.sortCenter) {
-			posts = posts.map(post => ({
-				distance: this.getDistance(this.props.sortCenter, post),
-				post: post
-			}));
-			console.log('Distances calculated', Date.now());
-			const sorted = posts
-				.sort((a, b) => a.distance - b.distance)
-				.map(obj => obj.post);
-
-			console.log('Sort complete', Date.now());
-			return sorted;
-		} else {
-			return posts;
-		}
-	};
+	_hideApplications = () =>
+		this.setState({
+			isApplicationsModalVisible: false
+		});
 
 	render() {
+		console.log('Rendering a PostList', this.props.listData.length);
 		return (
-			<View flex={1}>
+			<View>
 				<PostOrCenterModal
 					isVisible={this.state.isPostModalVisible}
 					post={this.state.selectedPost}
@@ -85,123 +54,19 @@ export default class PostList extends React.Component {
 					animationOut={'zoomOut'}
 				>
 					<ViewApplications
-						hide={() => {
-							this.setState({ isApplicationsModalVisible: false });
-						}}
+						hide={this._hideApplications}
 						post={this.state.selectedPost}
 					/>
 				</Modal>
-				{/* TODO use the FlatList instead. FlatLists can scroll
-					  TODO don't use that hacky Dimensions.get thing */}
-				<ScrollView
-					keyboardShouldPersistTaps={'handled'}
-					style={{ flex: 1, width: Dimensions.get('window').width }}
-				>
-					<FlatList
-						data={this._sort(this.props.listData)}
-						scrollEnabled={false}
-						ItemSeparatorComponent={() => <View style={SharedStyles.divider} />}
-						renderItem={({ item }) =>
-							<TouchableOpacity
-								onPress={() => {
-									this._showModal(item);
-								}}
-								style={styles.rowStyles}
-							>
-								<EventIcon item={item} />
 
-								<Text style={styles.rowText}>
-									{item.title}
-								</Text>
-
-								{item.owner === firebase.auth().currentUser.uid
-									? <TouchableOpacity
-											style={styles.applicationCounter}
-											onPress={() => {
-												this.setState({
-													isApplicationsModalVisible: true,
-													selectedPost: item
-												});
-											}}
-										>
-											<ApplicationCount applications={item.applications} />
-										</TouchableOpacity>
-									: null}
-								{item.dates
-									? // display date of event
-										<Text style={styles.dateText}>
-											{formatDate(getNextDate(item.dates)) +
-												(item.dates.length > 1 ? '...' : '')}
-										</Text>
-									: null}
-
-								{this.props.distanceCenter
-									? // display how far away the event is
-										<Text style={styles.distanceText}>
-											{Math.round(
-												this.getDistance(this.props.distanceCenter, item) / 1000
-											) + ' km'}
-										</Text>
-									: null}
-							</TouchableOpacity>}
-					/>
-					<View style={styles.empty}>
-						{this.props.message}
-					</View>
-				</ScrollView>
+				<BarePostList
+					listData={this.props.listData}
+					sortCenter={this.props.sortCenter}
+					distanceCenter={this.props.distanceCenter}
+					showModal={this._showModal}
+					showApplications={this._showApplications}
+				/>
 			</View>
 		);
 	}
 }
-
-// Stateless component! Read more about them at
-// https://medium.com/front-end-hacking/stateless-components-in-react-native-e9034f2e3701
-const ApplicationCount = ({ applications }) => {
-	const numApplications = Object.keys(applications).length;
-	return (
-		<Text style={styles.numAppText}>
-			{numApplications}{' '}
-			{translate(numApplications === 1 ? 'Application' : 'Applications')}
-		</Text>
-	);
-};
-
-const styles = StyleSheet.create({
-	rowStyles: {
-		flexDirection: 'row',
-		padding: 10
-	},
-	rowText: {
-		flex: 1,
-		fontSize: 19,
-		marginLeft: 10,
-		marginRight: 10,
-		color: 'black'
-	},
-	applicationCounter: {
-		justifyContent: 'center',
-		backgroundColor: '#658bcd',
-		borderRadius: 12,
-		height: 24,
-		alignItems: 'center',
-		width: 120
-	},
-	dateText: {
-		position: 'absolute',
-		right: 15,
-		bottom: 22
-	},
-	distanceText: {
-		position: 'absolute',
-		right: 15,
-		bottom: 3
-	},
-	empty: {
-		alignItems: 'center',
-		paddingTop: 10
-	},
-	numAppText: {
-		color: 'white',
-		backgroundColor: 'transparent'
-	}
-});
--- src/components/ViewPosts.js
+++ src/components/ViewPosts.js
@@ -1,5 +1,5 @@
 import { Location, Permissions } from 'expo';
-import { Platform, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import firebase from 'firebase';
 
