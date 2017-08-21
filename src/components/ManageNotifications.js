ab95b85355a5b7ef5d3a2a438d7f9a29f58a9216
Ethan Brown <esbrown@stanford.edu>
2017-08-21 22:21:24 +0200

    combined ManageNotifications and NewNotification into one modal.  Added safeguarding against Notification deletion.  Changed Styling

--- src/components/ManageNotifications.js
+++ src/components/ManageNotifications.js
@@ -4,7 +4,8 @@ import {
 	StyleSheet,
 	Text,
 	TouchableOpacity,
-	View
+	View,
+	Alert
 } from 'react-native';
 import Modal from 'react-native-modal';
 import React, { Component } from 'react';
@@ -47,110 +48,100 @@ export default class ManageNotifications extends Component {
 	}
 
 	_deleteSubscription(subscription) {
-		firebase.database().ref('subscriptions').child(subscription.key).remove();
+		Alert.alert(
+			translate('Are you sure you want to remove this notification?'),
+			translate('This cannot be undone'),
+			[
+				{ text: translate('No') },
+				{
+					text: translate('Yes'),
+					onPress: () => {
+						firebase
+							.database()
+							.ref('subscriptions')
+							.child(subscription.key)
+							.remove();
+					}
+				}
+			],
+			{ cancelable: false }
+		);
 	}
 
 	render() {
-		return (
-			<View style={styles.container}>
-				<Modal
-					isVisible={this.state.newNotificationVisible}
-					animationIn={'zoomIn'}
-					animationOut={'zoomOut'}
-				>
-					<NewNotification
-						hide={() => this.setState({ newNotificationVisible: false })}
-					/>
-				</Modal>
-
-				<ExitBar title={'Manage Notifications'} hide={this.props.hide} />
-
-				<View
-					style={{
-						flexDirection: 'row',
-						justifyContent: 'space-around',
-						marginTop: 10,
-						marginBottom: 5
-					}}
-				>
-					<View style={{ flex: 1 }} />
-					<Text style={styles.heading}>
-						{translate('Range')}
-					</Text>
-					<Text style={styles.heading}>
-						{translate('Location')}
-					</Text>
-					<View style={{ flex: 1 }} />
-				</View>
-
-				<View style={[SharedStyles.divider, { height: 3 }]} />
-				<FlatList
-					style={{ width: '100%' }}
-					data={Object.values(this.state.subscriptions)}
-					ItemSeparatorComponent={() => <View style={SharedStyles.divider} />}
-					renderItem={({ item }) =>
-						<View
-							key={item.key}
-							style={{
-								flexDirection: 'row',
-								justifyContent: 'space-around',
-								alignItems: 'center',
-								padding: 10
-							}}
-						>
-							<View style={{ alignItems: 'center', flex: 1 }}>
-								<Image
-									style={{
-										tintColor: Colors.blue.dark,
-										width: 50,
-										height: 50,
-										resizeMode: 'contain'
-									}}
-									source={{ uri: global.db.categories[item.icon].iconURL }}
-								/>
-
-								<Text
-									style={{
-										color: Colors.blue.dark,
-										fontSize: 10,
-										textAlign: 'center'
-									}}
-								>
-									{translate(global.db.categories[item.icon].title)}
-								</Text>
-							</View>
-
-							<Text style={{ fontSize: 16, flex: 1, textAlign: 'center' }}>
-								{item.radius + ' KM'}
-							</Text>
-							<Text style={{ fontSize: 12, flex: 2 }} numberOfLines={2}>
-								{item.formatted_address}
-							</Text>
-
-							<TouchableOpacity
-								onPress={this._deleteSubscription.bind(this, item)}
+		return this.state.newNotificationVisible
+			? <NewNotification
+					back={() => this.setState({ newNotificationVisible: false })}
+					hide={this.props.hide}
+				/>
+			: <View style={styles.container}>
+					<ExitBar title={'Manage Notifications'} hide={this.props.hide} />
+					<FlatList
+						style={{ width: '100%' }}
+						data={Object.values(this.state.subscriptions)}
+						ItemSeparatorComponent={() => <View style={SharedStyles.divider} />}
+						renderItem={({ item }) =>
+							<View
+								key={item.key}
 								style={{
-									backgroundColor: Colors.grey.light,
-									padding: 5,
-									borderRadius: 10,
-									marginLeft: 10
+									flexDirection: 'row',
+									justifyContent: 'space-around',
+									alignItems: 'center',
+									padding: 10
 								}}
 							>
-								<FontAwesome name={'trash-o'} size={26} />
-							</TouchableOpacity>
-						</View>}
-				/>
-				<View style={styles.bottomBar}>
-					<TouchableOpacity
-						style={styles.addCircle}
-						onPress={() => this.setState({ newNotificationVisible: true })}
-					>
-						<Entypo name={'plus'} size={44} style={styles.addIcon} />
-					</TouchableOpacity>
-					<Text style={styles.bottomText}>Add Notification</Text>
-				</View>
-			</View>
-		);
+								<View style={{ alignItems: 'center', flex: 1 }}>
+									<Image
+										style={{
+											tintColor: Colors.grey.dark,
+											width: 50,
+											height: 50,
+											resizeMode: 'contain'
+										}}
+										source={{ uri: global.db.categories[item.icon].iconURL }}
+									/>
+
+									<Text
+										style={{
+											color: Colors.grey.dark,
+											fontSize: 10,
+											textAlign: 'center'
+										}}
+									>
+										{translate(global.db.categories[item.icon].title)}
+									</Text>
+								</View>
+
+								<Text style={{ fontSize: 16, flex: 1, textAlign: 'center' }}>
+									{item.radius + ' km'}
+								</Text>
+								<Text style={{ fontSize: 12, flex: 2 }} numberOfLines={2}>
+									{item.formatted_address}
+								</Text>
+
+								<TouchableOpacity
+									onPress={this._deleteSubscription.bind(this, item)}
+									style={{
+										backgroundColor: Colors.grey.light,
+										padding: 5,
+										borderRadius: 10,
+										marginLeft: 10
+									}}
+								>
+									<FontAwesome name={'trash-o'} size={26} />
+								</TouchableOpacity>
+							</View>}
+					/>
+					<View style={styles.bottomBar}>
+						<TouchableOpacity
+							style={styles.addCircle}
+							onPress={() => this.setState({ newNotificationVisible: true })}
+						>
+							<Entypo name={'plus'} size={44} style={styles.addIcon} />
+							{/* <Text style={styles.bottomText}>Create New Notification</Text> */}
+						</TouchableOpacity>
+					</View>
+				</View>;
 	}
 }
 
@@ -161,34 +152,26 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: 'white'
 	},
-	heading: {
-		flex: 1,
-		fontSize: 18,
-		fontWeight: '500'
-	},
 	bottomBar: {
 		height: 70,
 		width: '100%',
 		backgroundColor: Colors.grey.light,
 		borderColor: Colors.grey.medium,
 		borderWidth: 1,
+		alignItems: 'center',
 		justifyContent: 'center'
 	},
 	addCircle: {
 		alignSelf: 'center',
 		height: 50,
-		width: 50,
-		backgroundColor: Colors.blue.dark,
-		borderRadius: 25,
+		width: '85%',
+		backgroundColor: '#4565A9',
+		borderRadius: 10,
 		justifyContent: 'center'
 	},
 	addIcon: {
 		color: 'white',
 		backgroundColor: 'transparent',
 		alignSelf: 'center'
-	},
-	bottomText: {
-		alignSelf: 'center',
-		fontSize: 10
 	}
 });
