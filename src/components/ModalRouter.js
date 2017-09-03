6bdf9d44672c860a924da130c0c5398f720b721f
benhannel <ben@bensprojects.com>
2017-09-03 14:11:24 +0200

    Attempt to fix Modal breaking issue, part 2

--- src/components/ModalRouter.js
+++ src/components/ModalRouter.js
@@ -19,18 +19,25 @@ export default class ModalRouter extends React.Component {
 			path: null,
 			props: null
 		};
+
+		this.lastPromise = Promise.resolve();
 	}
 
 	componentDidMount() {
 		global.setCurrentModal = (path, props) => {
-			// TODO log the props as well so we can see what post/application/article/
-			// whatever was viewed
-			const message = path
-				? `Showing Modal ${path}`
-				: `Hiding Modal ${this.state.path}`;
-			console.log(message);
-			Expo.Amplitude.logEvent(message);
-			this.setState({ path: path, props: props });
+			// Wait for the last state change to complete before we do the next one
+			this.lastPromise.then(() => {
+				// TODO log the props as well so we can see what post/application/article/
+				// whatever was viewed
+				const message = path
+					? `Showing Modal ${path}`
+					: `Hiding Modal ${this.state.path}`;
+				console.log(message);
+				Expo.Amplitude.logEvent(message);
+				this.lastPromise = new Promise((resolve, reject) => {
+					this.setState({ path: path, props: props }, resolve);
+				});
+			});
 		};
 	}
 
